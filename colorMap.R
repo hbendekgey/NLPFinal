4a35b1ac35d914732d6e0d4972ba44b0475fc455
hbendekgey <henry.bendekgey@pomona.edu>
2019-04-28 13:38:51 -0700

    added colorMap and better tokenizing

---
+++ colorMap.R
@@ -0,0 +1,28 @@
+library(ggplot2)
+library(wesanderson)
+library(ggpubr)
+library(dplyr)
+library(readr)
+
+dist <- read_csv("Desktop/NLPfinal/dist.tsv")
+
+x <- rep(c(1:186),186)
+y <- c()
+for (i in c(1:186)) {
+  y <- c(y, rep(i,186))
+}
+vals <- c(rep(0,186^2))
+k <- 1
+for (row in dist) {
+  for (j in c(1:186)) {
+    vals[k] <- row[j]
+    k <- k + 1
+  }
+}
+
+pal <- wes_palette(3, name="Zissou1", type="continuous") 
+val.df <- data.frame(x=x,y=y,c=vals)
+ggplot(val.df, aes(x=x,y=y)) + 
+  geom_raster(aes(fill = c)) + 
+  scale_fill_gradientn(colours=pal) + 
+  labs(x="",y="",fill="")
